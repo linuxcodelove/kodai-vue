1839e109b3a30b7dec31181237e01bdfc0bd6206
muthukumar <muthu@ardhika.com>
2022-11-05 11:53:25 +0530

    intial page

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,53 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-carousel hide-delimiters cycle interval="4000" height="600">
+      <v-app-bar app color="transparent" flat>
+        <v-btn text>KODAIKANAL TRAVEL PLANNER</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text>Home</v-btn>
+        <v-btn text>ROOMS</v-btn>
+        <v-btn text>FOOD</v-btn>
+        <v-btn text>ABOUT</v-btn>
+        <v-btn text>CONTACT</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="green">BOOK</v-btn>
+      </v-app-bar>
+      <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
+        <v-container fill-height fluid pa-0 ma-0>
+          <v-card class="pa-2 mx-auto" color="transparent" flat>
+            <span class="text-h2 white--text" style="text-align: center"
+              >Make yourself at home<br />in our hotel</span
+            >
+          </v-card>
+        </v-container>
+      </v-carousel-item>
+    </v-carousel>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    items: [
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+      },
+    ],
   }),
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,9 @@
 <template>
-  <hello-world />
+  <div>Homepage</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: "Home",
+};
 </script>
