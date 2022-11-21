34aabdf9ce08c49db866a9b12a68e46edb3f9969
muthukumar <linuxcodelove@gmail.com>
2022-11-21 23:38:49 +0530

    bugs fixed

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import * as VueGoogleMaps from "vue2-google-maps";
-import vuetify from "./plugins/vuetify";
-
-Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
   load: {
